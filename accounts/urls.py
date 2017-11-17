7557c5cdd2768590636eed8045b8ff6afb652086
Yusuf Hendricks <yhendricks1@gmail.com>
2017-11-17 22:40:41 +0200

    Video 20 complete

--- accounts/urls.py
+++ accounts/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     url(r'^register/$', views.register, name='register'),
     url(r'^profile/$', views.profile, name='profile'),
     url(r'^profile/edit/$', views.edit_profile, name='edit_profile'),
+    url(r'^change-password/$', views.change_password, name='change_password'),
 ]
