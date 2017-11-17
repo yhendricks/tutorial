a8b32352f1d2497836584b38aeeafcb27275975a
Yusuf Hendricks <yhendricks1@gmail.com>
2017-11-17 16:11:00 +0200

    Video 16 completed

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from .forms import RegistrationForm
 
 # Create your views here.
 
@@ -13,13 +14,13 @@ def home(request):
 
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = RegistrationForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect('/account')
 
     else:
-        form = UserCreationForm()
+        form = RegistrationForm()
 
         args = {'form': form}
         return render (request, 'accounts/reg_form.html', args)
