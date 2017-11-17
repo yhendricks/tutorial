4db54b3804d5848bda9611efe09ad48daedfc6b9
Yusuf Hendricks <yhendricks1@gmail.com>
2017-11-17 22:25:22 +0200

    Video 19 complete

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+
 
 class RegistrationForm(UserCreationForm):
     email = forms.EmailField(required=True)
@@ -26,3 +27,15 @@ class RegistrationForm(UserCreationForm):
             user.save()
 
         return user
+
+
+class EditProfileForm(UserChangeForm):
+
+    class Meta:
+        model = User
+        fields = (
+            'email',
+            'first_name',
+            'last_name',
+            'password'
+        )
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserChangeForm
-from .forms import RegistrationForm
+from .forms import RegistrationForm, EditProfileForm
 from django.contrib.auth.models import User
 # Create your views here.
 
@@ -33,13 +33,13 @@ def profile(request):
 
 def edit_profile(request):
     if request.method == 'POST':
-        form = UserChangeForm(request.POST, instance=request.user)
+        form = EditProfileForm(request.POST, instance=request.user)
 
         if form.is_valid():
             form.save()
             return redirect('/account/profile')
 
     else:
-        form = UserChangeForm(instance=request.user)
+        form = EditProfileForm(instance=request.user)
         args = {'form': form}
         return render(request, 'accounts/edit_profile.html', args)
