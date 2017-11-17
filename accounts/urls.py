fe161e15c7f34abfc155e2b1d6d65e4ae2c7b70c
Yusuf Hendricks <yhendricks1@gmail.com>
2017-11-17 22:46:18 +0200

    Video 21 complete

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
 from . import views
-from django.contrib.auth.views import login, logout
+from django.contrib.auth.views import login, logout, password_reset, password_reset_done
 
 urlpatterns = [
     url(r'^$', views.home),
@@ -10,4 +10,7 @@ urlpatterns = [
     url(r'^profile/$', views.profile, name='profile'),
     url(r'^profile/edit/$', views.edit_profile, name='edit_profile'),
     url(r'^change-password/$', views.change_password, name='change_password'),
+    url(r'^reset-password/$', password_reset, name="password_reset"),
+    url(r'^reset-password/done/$', password_reset_done, name='password_reset_done'),
+
 ]
