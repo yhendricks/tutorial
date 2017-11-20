7a6d14421d74f155eb378a8bcbc7f7fdff4310fe
Yusuf Hendricks <yhendricks1@gmail.com>
2017-11-20 11:11:56 +0200

    Completed video 24

--- accounts/urls.py
+++ accounts/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     url(r'^login/$', login, {'template_name': 'accounts/login.html'}),
     url(r'^logout/$', logout, {'template_name': 'accounts/logout.html'}),
     url(r'^register/$', views.register, name='register'),
-    url(r'^profile/$', views.profile, name='profile'),
+    url(r'^profile/$', views.view_profile, name='profile'),
     url(r'^profile/edit/$', views.edit_profile, name='edit_profile'),
     url(r'^change-password/$', views.change_password, name='change_password'),
     url(r'^reset-password/$', password_reset, name="password_reset"),
--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import PasswordChangeForm
 from .forms import RegistrationForm, EditProfileForm
 from django.contrib.auth import update_session_auth_hash
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 
@@ -26,7 +27,8 @@ def register(request):
         return render (request, 'accounts/reg_form.html', args)
 
 
-def profile(request):
+@login_required
+def view_profile(request):
     args = {'user': request.user}
     return render(request, 'accounts/profile.html', args)
 
