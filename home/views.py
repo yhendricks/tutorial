54406e51def288836407faad137f206b7e864fa1
Yusuf Hendricks <yhendricks1@gmail.com>
2017-11-21 15:03:17 +0200

    Video 45 completed

--- home/views.py
+++ home/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic import TemplateView
+from .forms import HomeForm
 
 # Create your views here.
-def home(request):
-    return HttpResponse('It works')
+#def home(request):
+ #   return HttpResponse('It works')
 
 
 class HomeView(TemplateView):
     template_name = 'home/home.html'
+
+    def get(self, request):
+        form = HomeForm()
+        return render(request, self.template_name, {'form': form})
