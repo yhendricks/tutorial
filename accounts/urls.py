ceb66cafee677960c86dd7f81aca04cd4351d9ec
Yusuf Hendricks <yhendricks1@gmail.com>
2017-11-21 09:36:44 +0200

    Video 32 completed

--- accounts/urls.py
+++ accounts/urls.py
@@ -15,8 +15,11 @@ urlpatterns = [
     url(r'^change-password/$', views.change_password, name='change_password'),
     url(r'^reset-password/$', password_reset, {
         'template_name': 'accounts/reset_password.html',
-        'post_reset_redirect': 'accounts:password_reset_done'}, name="password_reset"),
-    url(r'^reset-password/done/$', password_reset_done, name='password_reset_done'),
+        'post_reset_redirect': 'accounts:password_reset_done',
+        'email_template_name': 'accounts/reset_password_email.html'},
+        name="password_reset"),
+    url(r'^reset-password/done/$', password_reset_done,
+        {'template_name': 'accounts/reset_password_done.html'},name='password_reset_done'),
     url(r'^reset-password/confirm/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$',
         password_reset_confirm, name='password_reset_confirm'),
     url(r'^reset-password/complete/$', password_reset_complete, name='password_reset_complete'),
