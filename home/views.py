b5daab3b3b40a58059b7674690f33a94e477d8e9
Yusuf Hendricks <yhendricks1@gmail.com>
2017-11-22 10:10:31 +0200

    Video 27 completed

--- home/views.py
+++ home/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.views.generic import TemplateView
 from .forms import HomeForm
@@ -18,7 +18,13 @@ class HomeView(TemplateView):
     def post(self, request):
         form = HomeForm(request.POST)
         if form.is_valid():
+            post = form.save(commit=False)
+            post.user = request.user
+            post.save()
+
             text = form.cleaned_data['post']
+            form = HomeForm()
+            return redirect('home:home')
 
         args = {'form': form, 'text': text}
         return render(request, self.template_name, args)
